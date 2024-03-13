f37c6ecb602763117d3c6e16ca51b34bf0ef4bc5
JamConn <20085217>
2024-03-13 14:17:28 +0000

    Typed function signature.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -49,7 +49,7 @@ const friend1 = {
     },
   };
 
-  export const colleagues : ColleagueHistory = {
+  export const colleagues = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,53 @@
+import { friends, colleagues } from './01-basics'
 import {Friend, Colleague } from './myTypes'
+
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
+
+console.log(older(friends[0]))
+
+function allOlder(f: Friend[]) : string [] {
+    const result: string[] = f.map((f) => {
+        f.age += 1
+    return `${f.name} is now ${f.age}`;
+    });
+    return result;
+}
+
+
+console.log(allOlder(friends))
+
+
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+  console.log(highestExtension(colleagues.current));
+
+
+
+  function addColleague(colleagues: Colleague[], name: string, department: string, email: string): Colleague[] {
+
+    const highest = highestExtension(colleagues)
+  
+    // Assign the new colleague the highest extension number plus one
+    const newColleague: Colleague = {
+      name,
+      department,
+      contact: {
+        email,
+        extension: highest.contact.extension + 1,
+      },
+    };
+    const updatedColleagues = [...colleagues, newColleague];
+  
+    return updatedColleagues;
+  }
+
+
+  const updatedColleagues = addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+  console.log(updatedColleagues);
